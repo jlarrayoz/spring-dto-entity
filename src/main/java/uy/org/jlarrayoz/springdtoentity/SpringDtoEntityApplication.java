2b39f9636dd915b9d154c0efddff01aa453bc4b2
Juan Larrayoz <juan.larrayoz@gmail.com>
2023-12-08 23:22:40 -0300

    Commit inicial

---
+++ src/main/java/uy/org/jlarrayoz/springdtoentity/SpringDtoEntityApplication.java
@@ -0,0 +1,13 @@
+package uy.org.jlarrayoz.springdtoentity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDtoEntityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDtoEntityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uy/org/jlarrayoz/springdtoentity/SpringDtoEntityApplicationTests.java
@@ -0,0 +1,13 @@
+package uy.org.jlarrayoz.springdtoentity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDtoEntityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
